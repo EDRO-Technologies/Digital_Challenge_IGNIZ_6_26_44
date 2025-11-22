80675b48e9e392514e56a5c69f849c53acdf7d73
kato <unknown.255.galactic.acc@gmail.com>
2025-11-23 00:49:52 +0500

    feat: add 3 required endpoints and some fixes

--- back/src/modules/ngdu/ngdu.controller.ts
+++ back/src/modules/ngdu/ngdu.controller.ts
@@ -3,7 +3,12 @@ import type { NextFunction, Request, Response } from 'express';
 import { sendResponse } from '@/lib/reponse';
 import { HttpStatus } from '@/utils/enums/http-status';
 
-import type { GetGraphRequest } from './dto/request.dto';
+import type {
+  GetGraphRequest,
+  PostIsConnectedRequest,
+  PostObjectExistsRequest,
+  PostObjectNameRequest
+} from './dto/request.dto';
 
 import * as ngduService from './ngdu.service';
 
@@ -40,6 +45,45 @@ export async function getNgduGraph(
 ): Promise<void> {
   try {
     const result = await ngduService.getNgduGraph(req.body);
+    sendResponse(res, HttpStatus.OK, result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function objectExists(
+  req: Request<object, object, PostObjectExistsRequest>,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const result = await ngduService.objectExists(req.body);
+    sendResponse(res, HttpStatus.OK, result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function objectByName(
+  req: Request<object, object, PostObjectNameRequest>,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const result = await ngduService.objectByName(req.body);
+    sendResponse(res, HttpStatus.OK, result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function isConnected(
+  req: Request<object, object, PostIsConnectedRequest>,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const result = await ngduService.isConnected(req.body);
     sendResponse(res, HttpStatus.OK, result);
   } catch (error) {
     next(error);
