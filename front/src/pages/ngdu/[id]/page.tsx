891e0d0e8cde24a01a2756a47b146cb640978552
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2025-11-22 18:36:38 +0500

    react-flow

---
+++ front/src/pages/ngdu/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { AppShell, ScrollArea, Select } from "@mantine/core";
+import { ReactFlowProvider } from "@xyflow/react";
+
+import { Flow } from "./Flow";
+
+const NAVBAR_WIDTH = 300;
+
+export function NgduPage() {
+  return (
+    <AppShell navbar={{ width: NAVBAR_WIDTH, breakpoint: "xs" }}>
+      <AppShell.Navbar>
+        <AppShell.Section>
+          <Select placeholder='Pick value' data={["React", "Angular", "Vue", "Svelte"]} />
+        </AppShell.Section>
+
+        <AppShell.Section></AppShell.Section>
+
+        <AppShell.Section grow component={ScrollArea}></AppShell.Section>
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        <ReactFlowProvider>
+          <Flow />
+        </ReactFlowProvider>
+      </AppShell.Main>
+    </AppShell>
+  );
+}
+
+export default NgduPage;
