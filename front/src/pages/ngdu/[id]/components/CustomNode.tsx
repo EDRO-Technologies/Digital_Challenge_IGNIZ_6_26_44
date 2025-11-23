65acd4f15e9702a168b7d6077dbd1bcae3782dcc
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2025-11-23 09:48:30 +0500

    LAST

--- front/src/pages/ngdu/[id]/components/CustomNode.tsx
+++ front/src/pages/ngdu/[id]/components/CustomNode.tsx
@@ -16,11 +16,26 @@ const OBJECT_TYPE_MAP: Record<IObjectType, string> = {
   well: "Скважина"
 } as const;
 
+const COLOR_TYPE_MAP: Record<IObjectType, string> = {
+  cdng: "--mantine-color-grape-6",
+  kust: "--mantine-color-teal-6",
+  mest: "--mantine-color-yellow-6",
+  ngdu: "--mantine-color-grape-6",
+  obj: "--mantine-color-indigo-6",
+  plast: "--mantine-color-pink-6",
+  well: "--mantine-color-grape-6"
+} as const;
+
 export function CustomNode(props: NodeProps<CustomNodeProps>) {
   const [opened, { toggle }] = useDisclosure(false);
 
   return (
-    <Stack className='custom-node'>
+    <Stack
+      className='custom-node'
+      style={{
+        borderColor: `var(${COLOR_TYPE_MAP[props.data.type]})`
+      }}
+    >
       <Handle type='target' position={Position.Left} />
 
       <Group justify='space-between'>
